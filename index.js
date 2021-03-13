7777419a6e8b20bf8afd9d94227b1f1c8452b62a
Josh Nussbaum <joshnuss@gmail.com>
2021-03-13 01:24:14 -0500

    Adds websocket push notifications

--- index.js
+++ index.js
@@ -3,34 +3,71 @@ import glob from 'tiny-glob'
 import fs from 'fs'
 import path from 'path'
 import yaml from 'yaml'
+import http from 'http'
+import ws from 'ws'
+import chokidar from 'chokidar'
 
 const app = express()
-const content = await loadContent('content')
+const server = http.createServer(app)
+const wss = new ws.Server({server})
+const dir = 'content'
+const allWatches = {}
+const content = await loadContent(dir)
+
+chokidar.watch(dir).on('all', (event, path) => {
+  console.log(event, path)
+
+  if (event == 'change') {
+    loadFile(path, content)
+  }
+})
+
+wss.on('connection', ws => {
+  const watches = []
+
+  ws.on('message', message => {
+    parseKeys(message).forEach(key => {
+      key = key.join('.')
+
+      allWatches[key] ||= []
+      allWatches[key].push((key, value) => {
+        ws.send(JSON.stringify({key, value}))
+      })
+      watches.push(key)
+    })
+  })
+})
 
 async function loadContent(dir) {
   const files = await glob(path.join(dir, "**/*.yml"))
   const content = {}
 
   files.forEach(file => {
-    const keys = file
-      .replace(new RegExp(`^${dir}/`), '')
-      .split('/')
-      .map(key => path.parse(key).name)
-
-    const data = fs.readFileSync(file, 'utf8')
-    const record = yaml.parse(data)
-
-    const last = keys.reduce((acc, key) => {
-      if (!acc[key])
-        acc[key] = {}
-
-      return acc[key]
-    }, content)
-
-    Object.assign(last, record)
+    loadFile(file, content)
   })
 
   return content
+}
+
+function loadFile(file, content) {
+  const keys = file
+    .replace(new RegExp(`^${dir}/`), '')
+    .split('/')
+    .map(key => path.parse(key).name)
+
+  const callbacks = allWatches[keys.join('.')] || []
+  const data = fs.readFileSync(file, 'utf8')
+  const record = yaml.parse(data)
+
+  const last = keys.reduce((acc, key, index) => {
+    if (!acc[key] || index == keys.length-1)
+      acc[key] = {}
+
+    return acc[key]
+  }, content)
+
+  callbacks.forEach(callback => callback(keys.join('.'), record))
+  Object.assign(last, record)
 }
 
 function findResults(keys) {
@@ -51,7 +88,7 @@ function findResults(keys) {
     if (last && !missing) {
       let lastResult = results
       parts.forEach((part, i) => {
-        lastResult[part] ||= i == parts.length - 1 ? last : {} 
+        lastResult[part] ||= (i == parts.length - 1 ? last : {}) 
 
         lastResult = lastResult[part]
       })
@@ -61,11 +98,15 @@ function findResults(keys) {
   return results
 }
 
+function parseKeys(string) {
+  return string.split('+').map(key => key.split('.'))
+}
+
 app.get('/content/*', (req, res) => {
-  const keys = req.params[0].split('+').map(key => key.split('.'))
+  const keys = parseKeys(req.params[0])
   const results = findResults(keys)
 
   res.json(results)
 })
 
-app.listen(3001)
+server.listen(3001)
